1de6073e2711c7043df12b636a58ebb020d3b420
haarop <hannahakuol@gmail.com>
2026-07-13 21:32:02 +0300

    Create Patient class with basic attributes

---
+++ src/Patient.java
@@ -0,0 +1,11 @@
+public class Patient {
+
+    private int patientId;
+    private String fullName;
+    private String gender;
+    private String phone;
+    private String address;
+
+    public Patient() {
+    }
+}
