f83f0d070bddd6e434bbb73aaaf0b501d56538aa
haarop <hannahakuol@gmail.com>
2026-07-13 21:32:49 +0300

    Create Doctor class with basic attributes

---
+++ src/Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+
+    private int doctorId;
+    private String doctorName;
+    private String specialization;
+    private String phone;
+    private String email;
+
+    public Doctor() {
+    }
+}
