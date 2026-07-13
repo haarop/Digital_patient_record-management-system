71cfc32b4a1ae57cec20d970c65925f903c6de3c
haarop <hannahakuol@gmail.com>
2026-07-13 21:30:00 +0300

    Create Login.java

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+public class Login {
+
+    public Login() {
+        // Constructor
+    }
+
+    public boolean authenticate(String username, String password) {
+        // Authentication logic will be added later
+        return false;
+    }
+}
